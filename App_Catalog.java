89492a8df88a4a460ebf89cbe8392ec56f57470a
Manuel Tapia <manuel_30749@hotmail.com>
2017-07-28 15:42:37 -0500

    changes
    chnages

---
+++ App_Catalog.java
@@ -0,0 +1,56 @@
+package com.AppErrorReport.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.codehaus.jackson.annotate.JsonAutoDetect;
+
+@JsonAutoDetect
+@Entity
+@Table(name="APP_CATALOG")
+public class App_Catalog {	
+	
+	private int idApp;
+	private String App;
+	private boolean status;
+		
+	@Id
+	@GeneratedValue
+	@Column(name="IDAPP")
+	public int getIdApp() {
+		return idApp;
+	}
+	public void setIdApp(int idApp) {
+		this.idApp = idApp;
+	}
+	
+	@Column(name="APP", nullable=false)
+	public String getApp() {
+		return App;
+	}
+	public void setApp(String app) {
+		App = app;
+	}
+	
+	@Column(name="STATUS", nullable=false)
+	public boolean isStatus() {
+		
+		return status;
+	}	
+	public void setStatus(boolean status) {
+		
+		this.status = status;
+	}
+	
+	public App_Catalog() {
+		
+	}
+	
+	public App_Catalog(int idApp, String app) {
+		setIdApp(idApp);
+		setApp(app);
+	}
+}
